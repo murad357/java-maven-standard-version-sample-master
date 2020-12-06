9cd85cd3c7376a42e849a4d0e065f039cd0e6fea
Md Jashim Uddin <uddin.md.jashim@gmail.com>
2020-12-06 18:46:49 -0500

    feat: update yml file - feat! from 0.6.9

--- src/main/java/com/dwmkerr/javamaveenstandardversionsample/HelloWorld.java
+++ src/main/java/com/dwmkerr/javamaveenstandardversionsample/HelloWorld.java
@@ -7,6 +7,6 @@ package com.dwmkerr.javamaveenstandardversionsample;
 public class HelloWorld {
     public String Hello() {
         //  git commit -m 'updated the action workflow yml file 3' --no-verify
-        return "This is a semantic versioning test - feat";
+        return "This is a semantic versioning test - feat!";
     }
 }
